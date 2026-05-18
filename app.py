f0576b90ee2bd5cc7a6aa8b9ecf968afbfe40080
AlejitoB <144928721+AlejitoB@users.noreply.github.com>
2026-05-17 19:54:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,6 @@ def predecir():
     })
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
